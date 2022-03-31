91d4823586ff6d81c5b665b28e484b8b92eb5015
Vladlen Shaparev <shve@outlook.com>
2022-03-31 07:47:42 +0700

    Add project files.

---
+++ AskNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace Seconds
+{
+    internal static class AskNumber
+    {
+        internal static ulong Ask()
+        {
+            do
+            {
+                Console.WriteLine("Enter amount of seconds from begin of day.");
+                var userAnser = Console.ReadLine();
+                if (ulong.TryParse(userAnser, out ulong answer))
+                {
+                    return answer;
+                }
+                Console.WriteLine("Can't parse entered string.");
+            }
+            while (true);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Seconds;
+
+var amountOfSeconds = AskNumber.Ask();
+uint secondsInMinute = 60;
+uint secondsInHour = 60 * secondsInMinute;
+
+var hoursLeft = amountOfSeconds / secondsInHour;
+var minutesLeft = (amountOfSeconds - hoursLeft * secondsInHour) / secondsInMinute;
+var secondsLeft = amountOfSeconds - hoursLeft * secondsInHour - minutesLeft * secondsInMinute;
+
+Console.WriteLine($"{hoursLeft} hours from begin of the day.");
+Console.WriteLine($"{minutesLeft} minutes from begin of the hour");
+Console.WriteLine($"{secondsLeft} seconds from begin of the minute");
